facc4b44dd66252ff0e705e835e7f20e1a111116
dogrod <rodrickjue@hotmail.com>
2017-11-17 13:11:50 +0800

    Initial Commit

---
+++ ReactNativeIntegrationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactNativeIntegrationDemo
+//
+//  Created by Rodrick Zhu on 17/11/2017.
+//  Copyright © 2017 Rodrick Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactNativeIntegrationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReactNativeIntegrationDemo
+//
+//  Created by Rodrick Zhu on 17/11/2017.
+//  Copyright © 2017 Rodrick Zhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
